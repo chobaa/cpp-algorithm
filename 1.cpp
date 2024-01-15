0c83d3314da63ecf7bd99627be36eaf793dc1b69
chobaa <81730406+chobaa@users.noreply.github.com>
2024-01-15 16:59:05 +0900

    2024-01-15

--- 1.cpp
+++ 1.cpp
@@ -1,92 +1,73 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <vector>
 
 using namespace std;
-using ll = long long;
 
-int ar[100001];
-int n;
-
-vector<int> seg_idx;
-
-inline int mid(int s, int e) { return s + (e - s) / 2; }
-
-int cmpidx(int a, int b)
+void update(vector<int> &tree, int node, int start, int end, int index, int diff)
 {
-    if (a == -1)
-        return b;
-    if (b == -1)
-        return a;
-    if (ar[a] < ar[b])
-        return a;
-    return b;
+    if (!(start <= index && index <= end))
+        return;
+
+    tree[node] += diff;
+
+    if (start != end)
+    {
+        int mid = (start + end) / 2;
+
+        update(tree, node * 2, start, mid, index, diff);
+        update(tree, node * 2 + 1, mid + 1, end, index, diff);
+    }
 }
 
-int init_min(int node, int start, int end)
+int query(vector<int> &tree, int node, int start, int end, int val)
 {
+    int get;
+    int mid = (start + end) / 2;
+
     if (start == end)
-        return seg_idx[node] = end;
-    int m = mid(start, end);
-    int l = init_min(node * 2, start, m);
-    int r = init_min(node * 2 + 1, m + 1, end);
-    return seg_idx[node] = cmpidx(l, r);
-}
+    {
+        tree[node]--;
+        return start;
+    }
 
-int query_min(int node, int start, int end, int l, int r)
-{
-    if (start > r || end < l)
-        return -1;
-    if (l <= start && end <= r)
-        return seg_idx[node];
-    int m = mid(start, end);
-    return cmpidx(
-        query_min(node * 2, start, m, l, r),
-        query_min(node * 2 + 1, m + 1, end, l, r));
-}
+    if (tree[node * 2] >= val)
+        get = query(tree, node * 2, start, mid, val);
+    else
+        get = query(tree, node * 2 + 1, mid + 1, end, val - tree[node * 2]);
 
-ll init_sum(vector<ll> &tree, int node, int start, int end)
-{
-    if (start == end)
-        return tree[node] = ar[end];
-    int m = mid(start, end);
-    return tree[node] = init_sum(tree, node * 2, start, m) + init_sum(tree, node * 2 + 1, m + 1, end);
+    tree[node]--;
+    return get;
 }
-
-ll query_sum(vector<ll> &tree, int node, int start, int end, int l, int r)
-{
-    if (start > r || l > end)
-        return 0;
-    if (l <= start && end <= r)
-        return tree[node];
-    int m = mid(start, end);
-    return query_sum(tree, node * 2, start, m, l, r) + query_sum(tree, node * 2 + 1, m + 1, end, l, r);
-}
-
-ll query(vector<ll> &tree, int start, int end)
-{
-    if (start == end)
-        return (ll)ar[end] * ar[end];
-    int idx = query_min(1, 1, n, start, end);
-    ll res = ar[idx] * query_sum(tree, 1, 1, n, start, end);
-    if (start < idx)
-        res = max(res, query(tree, start, idx - 1));
-    if (idx < end)
-        res = max(res, query(tree, idx + 1, end));
-    return res;
-}
-
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    int n;
+    scanf("%d", &n);
 
-    cin >> n;
+    vector<int> arr(n + 1);
 
-    seg_idx.resize(n * 4);
-    vector<ll> sum_tree(n * 4);
+    int h = (int)ceil(log2(n));
+    int tree_size = 1 << (h + 1);
 
-    for (int i = 1; i <= n; ++i)
-        cin >> ar[i];
-    init_min(1, 1, n);
-    init_sum(sum_tree, 1, 1, n);
-    cout << query(sum_tree, 1, n);
+    vector<int> tree(tree_size);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+        update(tree, 1, 0, n - 1, i, 1);
+    }
+
+    vector<int> ans(n + 1);
+    for (int i = n - 1; i >= 0; i--)
+    {
+        int get = query(tree, 1, 0, n - 1, arr[i] + 1);
+
+        ans[get + 1] = i + 1;
+    }
+
+    for (int i = n; i >= 1; i--)
+        printf("%d ", ans[i]);
+    return 0;
 }
