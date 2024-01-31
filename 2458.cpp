3ee0ba7c517e72ad31450edaf4bde19bc220288b
chobaa <81730406+chobaa@users.noreply.github.com>
2024-01-31 14:47:47 +0900

    2024-01-31
    floyd, union-find

---
+++ 2458.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int map[501][501];
+
+int n, m;
+
+void floyd()
+{
+    for (int k = 1; k <= n; k++)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (map[i][j] > map[i][k] + map[k][j])
+                {
+                    map[i][j] = map[i][k] + map[k][j];
+                }
+            }
+        }
+    }
+}
+
+int main()
+{
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            map[i][j] = 987654321;
+        }
+    }
+    for (int i = 1; i <= m; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        map[a][b] = 1;
+    }
+    int answer = 0;
+    floyd();
+    for (int i = 1; i <= n; i++)
+    {
+        int cnt = 0;
+        for (int j = 1; j <= n; j++)
+        {
+            if (map[i][j] != 987654321 || map[j][i] != 987654321)
+                cnt++;
+            if (cnt == n - 1)
+                answer++;
+        }
+    }
+    cout << answer;
+}
