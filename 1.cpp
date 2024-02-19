e0b6efb7ac62374f6c5f6a1e0a7239fd93e29279
chobaa <81730406+chobaa@users.noreply.github.com>
2024-02-19 16:53:54 +0900

    2024-02-19
    LCA, greedy

--- 1.cpp
+++ 1.cpp
@@ -1,97 +1,119 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <string>
-#include <sstream>
-#include <cmath>
-#include <queue>
-
+#include <cstdio>
+#include <string.h>
 using namespace std;
 
-int map[101][101] = { 0, };
+int node_num;
+const int TREE_HIGHT = 20;
+const int MAX_NUM = 100000;
+int depth[MAX_NUM];
+int parent[MAX_NUM + 1][TREE_HIGHT];
+int min_road[MAX_NUM + 1][TREE_HIGHT];
+int max_road[MAX_NUM + 1][TREE_HIGHT];
+vector<pair<int, int>> adj[MAX_NUM + 1];
 
-int main()
+void FindParent(int par, int now, int dep, int road_len)
 {
-	queue<pair<int, int>> qSnake;
-	queue<pair<int, char>> qDir;
 
-	int N, K;
-	cin >> N;
-	cin >> K;
+	depth[now] = dep;
+	parent[now][0] = par;
+	min_road[now][0] = max_road[now][0] = road_len;
 
-	for (int i = 0; i < K; i++)
+	for (int i = 0; i < adj[now].size(); ++i)
 	{
-		int input_x, input_y;
-		cin >> input_x >> input_y;
-		map[input_x][input_y] = 1;
+		if (adj[now][i].first != par)
+			FindParent(now, adj[now][i].first, dep + 1, adj[now][i].second);
 	}
+	return;
+}
 
-	int L;
-	cin >> L;
+pair<int, int> FindMinMaxRoad(int a, int b)
+{
+	int min_result = 1000001, max_result = 0;
 
-	for (int i = 0; i < L; i++)
+	if (depth[a] != depth[b])
 	{
-		int time;
-		char dir;
+		if (depth[a] < depth[b]) // 깊이가 다르다면 a가 항상 더 깊게
+			swap(a, b);
 
-		cin >> time >> dir;
+		int dif = depth[a] - depth[b];
 
-		qDir.push(make_pair(time, dir));
-	}
-
-	int x = 1;
-	int y = 2;
-	int nCurDir = 1; //0: 아래 1: 오른쪽 2: 위 3: 왼쪽
-	int cnt = 1;
-
-	int dirX[4] = { 1, 0, -1, 0 };
-	int dirY[4] = { 0, 1, 0, -1 };
-
-	qSnake.push(make_pair(1, 1));	//초기 뱀 모양
-	map[1][1] = 2;
-
-	while (1)
-	{
-		if (x > N || y > N || x < 1 || y < 1)
-			break;
-
-		if (map[x][y] == 2)
-			break;
-
-		if (!qDir.empty())
+		for (int i = 0; dif > 0; ++i)
 		{
-			if (cnt == qDir.front().first)
+			if (dif % 2 == 1)
 			{
-				if (qDir.front().second == 'L') //왼쪽으로 회전
-					nCurDir = (nCurDir + 1) % 4;
-				else if (qDir.front().second == 'D') //오른쪽으로 회전
-					nCurDir = (nCurDir + 3) % 4;
+				min_result = min(min_result, min_road[a][i]);
+				max_result = max(max_result, max_road[a][i]);
+				a = parent[a][i];
+			}
+			dif = dif >> 1;
+		}
+	}
 
-				qDir.pop();
+	if (a != b)
+	{
+		for (int k = TREE_HIGHT - 1; k >= 0; --k)
+		{
+			if (parent[a][k] != 0 && parent[a][k] != parent[b][k])
+			{
+				min_result = min(min_result, min_road[a][k]);
+				min_result = min(min_result, min_road[b][k]);
+
+				max_result = max(max_result, max_road[a][k]);
+				max_result = max(max_result, max_road[b][k]);
+				a = parent[a][k];
+				b = parent[b][k];
 			}
 		}
 
-		if (map[x][y] == 0)
-		{
-			qSnake.push(make_pair(x, y));
-			map[qSnake.front().first][qSnake.front().second] = 0;
-			qSnake.pop();
-		}
+		min_result = min(min_result, min_road[a][0]);
+		min_result = min(min_result, min_road[b][0]);
 
-		else if (map[x][y] == 1)
-		{
-			qSnake.push(make_pair(x, y));
-		}
-
-		map[x][y] = 2;
-
-		x = x + dirX[nCurDir];
-		y = y + dirY[nCurDir];
-
-		cnt++;
+		max_result = max(max_result, max_road[a][0]);
+		max_result = max(max_result, max_road[b][0]);
 	}
-	
-	cout << cnt;
+
+	return make_pair(min_result, max_result);
+}
+
+int main()
+{
+	scanf("%d", &node_num);
+
+	int a, b, road;
+	for (int i = 0; i < node_num - 1; ++i)
+	{
+		scanf("%d %d %d", &a, &b, &road);
+		adj[a].push_back(make_pair(b, road));
+		adj[b].push_back(make_pair(a, road));
+	}
+
+	memset(parent, 0, sizeof(parent));
+	memset(min_road, 1000001, sizeof(min_road));
+	memset(max_road, 0, sizeof(max_road));
+
+	FindParent(0, 1, 0, 0);
+
+	for (int k = 1; k < TREE_HIGHT; ++k)
+		for (int idx = 2; idx <= node_num; ++idx)
+			if (parent[idx][k - 1] != 0)
+			{
+				parent[idx][k] = parent[parent[idx][k - 1]][k - 1];
+				min_road[idx][k] = min(min_road[parent[idx][k - 1]][k - 1], min_road[idx][k - 1]);
+				max_road[idx][k] = max(max_road[parent[idx][k - 1]][k - 1], max_road[idx][k - 1]);
+			}
+
+	int pair_num;
+	pair<int, int> result;
+	scanf("%d", &pair_num);
+
+	while (pair_num--)
+	{
+		scanf("%d %d", &a, &b);
+		result = FindMinMaxRoad(a, b);
+		printf("%d %d\n", result.first, result.second);
+	}
 
 	return 0;
 }
