e2bf694acb459330b3f4002dc12f25d22442bd16
chobaa <81730406+chobaa@users.noreply.github.com>
2024-03-18 15:48:16 +0900

    2024-03-18
    greedy & priority_queue

--- 1.cpp
+++ 1.cpp
@@ -1,80 +1,48 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
-#define MAX 30001
-#define MAX_K 15
+
 using namespace std;
+int n,ts,m;
+int ans;
+typedef pair<pair<int, int>, int> p;
 
-int N, M, a, b, pre = 1, now, ans = 0;
-bool visit[MAX];
-int dp[MAX][MAX_K];
-vector<int> tree[MAX];
-int depth[MAX], dist[MAX];
-
-void dfs(int n, int d)
-{
-	depth[n] = d;	 // depth 저장
-	visit[n] = true; // 방문
-	for (int i = 0; i < tree[n].size(); i++)
-	{
-		int next = tree[n][i]; // 다음 방문할 node
-		if (visit[next])
-			continue;	  // 이미 방문한 경우
-		dp[next][0] = n;  // 부모 정보 저장
-		dfs(next, d + 1); // dfs 탐색
-	}
+bool cmp(p a, p b) {
+    return a.first.second < b.first.second;
 }
 
-void init()
-{ // 2^k번째 조상까지 정보 저장
-	for (int k = 1; k < MAX_K; k++)
-	{
-		for (int i = 1; i <= N; i++)
-			dp[i][k] = dp[dp[i][k - 1]][k - 1];
-	}
-}
+int main() {
+    cin >> n >> ts >> m;
+    vector<p> arr;
+    vector<int> truck(n);
 
-int lca(int x, int y)
-{
-	if (depth[y] < depth[x])
-		swap(y, x); // y가 더 깊음을 보장
-	for (int k = MAX_K - 1; k >= 0; k--)
-	{ // x, y의 depth를 맞춤
-		if (depth[y] - depth[x] >= 1 << k)
-			y = dp[y][k];
-	}
-	if (x == y)
-		return x; // x가 LCA(최소 공통 조상)인 경우
-	for (int k = MAX_K - 1; k >= 0; k--)
-	{ // LCA를 찾음
-		if (dp[x][k] != dp[y][k])
-		{
-			x = dp[x][k];
-			y = dp[y][k];
-		}
-	}
-	return dp[x][0];
-}
+    for(int i=0 ; i<m ; i++) {
+        int a,b,c;
+        cin >> a >> b >> c;
+        arr.push_back(p(make_pair(a, b), c));
+    }
+    sort(arr.begin(), arr.end(), cmp);
+    
+    int size = arr.size();
+    for(int i=0 ; i<size ; i++) {
+        int start = arr[i].first.first-1;
+        int end = arr[i].first.second-1;
+        int box = arr[i].second;
+        int temp=0;
+        int cnt=0;
 
-int main()
-{
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> N;
-	for (int i = 1; i < N; i++)
-	{ // tree를 만듦
-		cin >> a >> b;
-		tree[a].push_back(b);
-		tree[b].push_back(a);
-	}
-	dfs(1, 0); // depth, 부모노드 정보 저장
-	init();	   // 2^k번째 조상까지의 정보 저장
-	cin >> M;
-	for (int i = 0; i < M; i++)
-	{ // 한동이가 방문해야 할 모든 도시를 방문 할 수 있는 최소 시간을 구함
-		cin >> now;
-		ans += (depth[pre] + depth[now] - 2 * depth[lca(now, pre)]);
-		pre = now;
-	}
-	cout << ans;
+        for(int j=start ; j<end ; j++) {
+            temp = max(temp, truck[j]); // 해당하는 마을에서 쌓인 박스 수 중 가장 큰 수
+        }
+        if(temp+box<=ts) cnt = box; // 가장 큰 수에 박스를 넣어도 용량을 안넘으면
+        else cnt = ts-temp; // 용량을 넘으면
+
+        for(int j=start ; j<end ; j++) {
+            truck[j] += cnt;
+        }
+        ans += cnt;
+    }
+
+    cout << ans << "\n";
+
+    return 0;
 }
